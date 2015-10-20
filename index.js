c056d2da87b7d7fc229c731ee804fc81a8f0b7c7
Leonhard <x32000@gmail.com>
2015-10-20 12:42:07 +0200

    Simple socket.io chat app

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var app = require('express')()
 var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/static/index.html')
+})
+
+io.on('connection', function (socket) {
+  console.log('New connection')
+  socket.on('disconnect', function () {
+    console.log('User disconnected')
+  })
+
+  socket.on('chat message', function (msg) {
+    console.log('message: ' + msg)
+  })
 })
 
 http.listen(3000, function () {
